e7c3bd5bf40b4443f7c987b18db4d6ddfc261a31
RaghaHanuma22 <raghahanuma@gmail.com>
2025-09-07 20:02:10 -0700

    Adding updated modules

--- Design/read_ptr.sv
+++ Design/read_ptr.sv
@@ -8,20 +8,20 @@ module read_ptr_handler #(parameter ptr_width = 6)(
     output logic empty
 );
 
-//logic [ptr_width-1 :0] b_rptr_next;
-//logic [ptr_width-1 :0] g_rptr_next;
-//logic rempty;
+logic [ptr_width-1 :0] b_rptr_next;
+logic [ptr_width-1 :0] g_rptr_next;
+logic rempty;
 
-assign b_rptr = !rrst_n?6'b001010:b_rptr + (r_en & ~empty & b_rptr != 6'b110101);
-assign g_rptr = !rrst_n?6'b001111:(b_rptr) ^ (b_rptr>>1);
-assign empty = (g_wptr_sync == g_rptr);
+assign b_rptr_next = b_rptr + ((r_en & ~empty) && (b_rptr != 6'b110101));
+assign g_rptr_next = (b_rptr_next) ^ (b_rptr_next>>1);
+assign rempty = (g_wptr_sync == g_rptr_next);
 
-/*
+
 always @(posedge rclk or negedge rrst_n) begin
     if (!rrst_n) begin
         b_rptr <= 6'b001010;
-        g_rptr <= 6'b001111;
-        //empty <= 1;
+        //g_rptr <= 6'b001111;
+        empty <= 1;
     end 
     else begin
         //b_rptr <= b_rptr + 1;
@@ -30,6 +30,6 @@ always @(posedge rclk or negedge rrst_n) begin
         g_rptr <= g_rptr_next;
         empty <= rempty;
     end
-end*/
+end
 
 endmodule
