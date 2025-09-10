b4878888f61b1f0a4033e4b6a16aba3355524200
Raghavendra Davarapalli <ragha@deimos.ece.pdx.edu>
2025-09-10 00:06:56 -0700

    Updating new modules

--- Design/fifo_mem.sv
+++ Design/fifo_mem.sv
@@ -1,14 +1,14 @@
 //Module for fifo memory
 //Author: Raghav
 
-module fifo_mem #(parameter data_width = 8,depth = 45) (
+module fifo_mem #(parameter data_width = 8,depth = 54) ( //Actual fifo_depth = 45 only
 input logic w_clk,r_clk,w_en,r_en,full,empty,arstn,
 input logic [data_width-1:0] data_in,
 output logic [data_width-1:0] data_out,
 input logic [5:0] w_ptr,r_ptr
 );
 
-logic [data_width-1:0] f_mem [depth-1:0];
+logic [data_width-1:0] f_mem [depth-1:9];
 
 //Logic for write side
 always_ff @(posedge w_clk or negedge arstn) begin
@@ -19,7 +19,6 @@ always_ff @(posedge w_clk or negedge arstn) begin
     end
     else if(w_en && ~full) begin
         f_mem[w_ptr] <= data_in;
-        $display("Write ptr = %d", w_ptr);
     end
 end
 
@@ -27,7 +26,6 @@ end
 always_ff @(posedge r_clk) begin
     if(~empty && r_en) begin
         data_out <= f_mem[r_ptr];
-        $display("Write ptr = %d", w_ptr);
     end
 end
 
